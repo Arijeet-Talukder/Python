c37fde45607dd62263486ce4e2e3b002e66ef555
Arijeet-Talukder <talukder.arko@gmail.com>
2026-02-24 20:30:46 +0600

    fixed bugs

---
+++ Prac.py
@@ -0,0 +1,12 @@
+import math
+def run():
+    prompt = int(input("Enter a number: ").strip())
+    
+    print(f"Next number is {prompt}")
+    m=math.ceil(10.0)
+    print(m)
+
+
+
+if __name__ == "__main__":
+    run()
